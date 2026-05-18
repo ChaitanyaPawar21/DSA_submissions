e2907f33a9a8d2d46ec8639b7c727ca7b678e933
rapt0r <chaitanya21pawar@gmail.com>
2026-05-18 19:01:19 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>map;
+        if(s.size()!=t.size()) return false;
+        for(int i=0;i<s.size();i++){
+            map[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            map[t[i]]--;
+        }
+        for(auto it: map){
+            if(it.second!=0){
+                return false;
+            }
+        }return true;
+    }
+};
